8f0250161e94499a58752eaa1bf83207ddd54005
Adhasetiawan <adhasetiawanwiyana@gmail.com>
2022-06-21 19:19:03 +0800

    first commit

---
+++ app/src/main/java/com/example/mobilepresence/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mobilepresence
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.mobilepresence.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firstFunction()
+    }
+
+    fun firstFunction(){
+        binding.btnClickMe.setOnClickListener {
+            Toast.makeText(applicationContext, "Mobile Presence Remake", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
